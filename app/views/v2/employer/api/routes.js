6d6aa4def10dfbd08962c4e2e2b9fc5774901db4
rob.langdon@digital.education.gov.uk <rob.langdon@digital.education.gov.uk>
2021-11-16 14:23:09 +0000

    #128 - subscriptions

--- app/views/v2/employer/api/routes.js
+++ app/views/v2/employer/api/routes.js
@@ -1,10 +1,2 @@
-// Confirm key renewal
-router.post('api-confirm-key', (req, res) => {
-	if (req.session.data['confirm-key'] == 'yes' ) {
-		res.redirect('api-hub-new-key')
-	} else {
-		res.redirect('api-hub')
-	}
-})
 
 module.exports = router
