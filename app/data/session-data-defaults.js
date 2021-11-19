10c0c0bf2fa3bd5c30268571ab56d6496623265f
rob.langdon@digital.education.gov.uk <rob.langdon@digital.education.gov.uk>
2021-11-19 09:54:48 +0000

    Employers - fixed menu and Dev portal - removed subscribe journey

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -16,9 +16,19 @@ Example usage:
 ============================================================================
 
 */
+//var moment = require('moment');
+var locations = require('./locations.json')
+var training = require('./training.json')
+var training_full = require('./training-full.json')
+var vacancies = require('./vacancies.json')
+var employers = require('./employers.json')
+var vacancies_new = require('./vacancies-new.json')
+var providers = require('./providers.json')
+var providers_full = require('./providers_full.json')
+var profanities = require('./profanities.json')
 
 module.exports = {
 
   // Insert values here
-
+  "providerName": "APEX TRAINING LIMITED",
 }
