0000320c38454b4450ec2c28fc9bb0672bd8c441
rob.langdon@digital.education.gov.uk <rob.langdon@digital.education.gov.uk>
2021-12-09 21:37:45 +0000

    v3 - Creating an apprenticeship vacancy content amendment

--- app/config.js
+++ app/config.js
@@ -13,8 +13,7 @@ module.exports = {
   port: '3000',
 
   // Enable or disable password protection on production
-  //useAuth: 'true',
-  useAuth: 'false',
+  useAuth: 'true',
 
   // Automatically stores form data, and send to all views
   useAutoStoreData: 'true',
