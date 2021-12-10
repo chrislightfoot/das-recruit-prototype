a51adf03cc39a5edd2db58c6ad1b908cd2dc1c90
rob.langdon@digital.education.gov.uk <rob.langdon@digital.education.gov.uk>
2021-12-10 11:26:44 +0000

    v3 - Recruit collection page - content update

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,60 @@
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+})
+
+function nodeListForEach (nodes, callback) {
+  if (window.NodeList.prototype.forEach) {
+    return nodes.forEach(callback)
+  }
+  for (var i = 0; i < nodes.length; i++) {
+    callback.call(window, nodes[i], i, nodes);
+  }
+}
+
+function Copy ($module) {
+  this.$module = $module
+}
+
+Copy.prototype.init = function () {
+  var $module = this.$module
+  if (!$module) {
+    return
+  }
+  var $button = document.createElement('button')
+  $button.className = 'app-copy-button js-copy-button'
+  $button.setAttribute('aria-live', 'assertive')
+  $button.textContent = 'Copy key'
+
+  $module.insertBefore($button, $module.firstChild)
+  this.copyAction()
+}
+Copy.prototype.copyAction = function () {
+  // Copy to clipboard
+  try {
+    new ClipboardJS('.js-copy-button', {
+      target: function (trigger) {
+        return trigger.nextElementSibling
+      }
+    }).on('success', function (e) {
+      e.trigger.textContent = 'Key copied'
+      e.clearSelection()
+      setTimeout(function () {
+        e.trigger.textContent = 'Copy key'
+      }, 5000)
+    })
+  } catch (err) {
+    if (err) {
+      console.log(err.message)
+    }
+  }
+}
+
+var $codeBlocks = document.querySelectorAll('[data-module="app-copy"]')
+nodeListForEach($codeBlocks, function ($codeBlock) {
+  new Copy($codeBlock).init()
+})
